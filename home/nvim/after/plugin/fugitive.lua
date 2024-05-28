63976c9e70b3c5f716e1a6c98a0a4a4c88535d63
Sny Spyper <snuspuper@example.com>
2024-05-28 09:34:18 +0300

    Added some fugitive binds

--- home/nvim/after/plugin/fugitive.lua
+++ home/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,5 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+local kmset = vim.keymap.set
+local vc = vim.cmd
+
+kmset("n", "<leader>gs", vc.Git)
+kmset("n", "<leader>gp", ":Git push origin master")
